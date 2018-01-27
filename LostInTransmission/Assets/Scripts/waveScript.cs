e18b7ee6c8f86c36d96a334b0a5b961c38419072
BMarcelus <bmdizon@uci.edu>
2018-01-27 09:39:10 -0800

    wave particle tests

---
+++ LostInTransmission/Assets/Scripts/waveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveScript : MonoBehaviour {
+
+    private float life = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.rotation * Vector3.down / 10;
+        life -= Time.deltaTime;
+        if (life<=0) {
+            Destroy(gameObject);
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ground"){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LostInTransmission/Assets/Scripts/waveSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.X)) {
+			for (int i = 0; i < 360; i++)
+			{
+				Instantiate(prefab, transform.position, Quaternion.Euler(0, 0, i));
+			}
+        }
+	}
+}
