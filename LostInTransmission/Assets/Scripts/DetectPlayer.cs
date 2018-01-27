72ce4a2d4b7874a4f1fd24a749d1c7fc4e4f5c74
Kyle Wong <kylewonga@gmail.com>
2018-01-27 12:38:48 -0800

    Added moving platforms and elevator

---
+++ LostInTransmission/Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    private State state;
+    public  int DETECTED = 1;
+    public int NOT_DETECTED = 0;
+	void Start () {
+        state = GetComponent<State>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+            state.setState(DETECTED);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            state.setState(DETECTED);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            state.setState(NOT_DETECTED);
+    }
+}
---
+++ LostInTransmission/Assets/Scripts/RaiseCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseCollision : MonoBehaviour {
+
+    // Use this for initialization
+    public bool onEnter;
+    public bool onStay;
+    public bool onExit;
+    public GameObject target;
+    public LayerMask layerToDetect;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (onEnter && layerToDetect == (layerToDetect | (1 << collision.gameObject.layer)))
+            target.SendMessage("OnChildTriggerEnter2D", collision);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (onStay && layerToDetect == (layerToDetect | (1 << collision.gameObject.layer)))
+            target.SendMessage("OnChildTriggerStay2D", collision);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (onExit && layerToDetect == (layerToDetect | (1 << collision.gameObject.layer)))
+            target.SendMessage("OnChildTriggerExit2D", collision);
+    }
+}
