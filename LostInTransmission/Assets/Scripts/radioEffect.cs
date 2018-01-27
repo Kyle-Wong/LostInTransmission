30c5f3ff0d83000f8293f8e23fa3a47e373fc80e
BMarcelus <bmdizon@uci.edu>
2018-01-27 11:22:36 -0800

    simple radio effect

---
+++ LostInTransmission/Assets/Scripts/radioEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radioEffect : MonoBehaviour {
+
+    private Vector2 size;
+	// Use this for initialization
+	void Start () {
+        size = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0,0,6));
+        float scale = 1+Mathf.Cos(Time.time * 4)/10;
+        transform.localScale = new Vector2(size.x * scale, size.y * scale);
+	}
+}
