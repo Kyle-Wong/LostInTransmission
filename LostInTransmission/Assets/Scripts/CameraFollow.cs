f1fdb8ac7f231eb45e5c205b90592fbf48ccf6fe
PGrad <paulgrad3rd@gmail.com>
2018-01-27 13:17:56 -0800

    Added material back onto osc_screen prefab, draft of camera follow

---
+++ LostInTransmission/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform currentPlayerTransform;
+
+	void Awake() {
+		Debug.Assert(currentPlayerTransform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var playerPos = currentPlayerTransform.position;
+		transform.position = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+	}
+}
