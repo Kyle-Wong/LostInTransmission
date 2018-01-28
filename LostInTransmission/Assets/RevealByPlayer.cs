93224a1e9862bd9d14ff8768d686729e826251e1
Kyle Wong <kylewonga@gmail.com>
2018-01-28 13:11:18 -0800

    Added fonts and minimum stay timer

--- LostInTransmission/Assets/RevealByPlayer.cs
+++ LostInTransmission/Assets/RevealByPlayer.cs
@@ -6,27 +6,42 @@ public class RevealByPlayer : MonoBehaviour {
 
     // Use this for initialization
     TextMeshColorLerp colorLerp;
+    private bool revealText = false;
     private bool dontRevealAgain = false;
+    private float stayDuration = 3f;
+    private float stayTimer;
 	void Start () {
         colorLerp = transform.parent.gameObject.GetComponent<TextMeshColorLerp>();
+        stayTimer = stayDuration;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (stayTimer > 0 && revealText)
+        {
+            stayTimer -= Time.deltaTime;
+            colorLerp.startColorChange(1);
+
+        } else if(stayTimer <= 0 && revealText)
+        {
+            revealText = false;
+            colorLerp.startColorChange(-1);
+            dontRevealAgain = true;
+
+        }
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !dontRevealAgain)
         {
-            colorLerp.startColorChange(1);
+            revealText = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            colorLerp.startColorChange(-1);
             dontRevealAgain = true;
         }
     }
