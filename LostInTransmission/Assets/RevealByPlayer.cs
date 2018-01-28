925764226071846397569bd9eabc0fbef94294d9
Kyle Wong <kylewonga@gmail.com>
2018-01-28 12:14:36 -0800

    Added poem line

---
+++ LostInTransmission/Assets/RevealByPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealByPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    TextMeshColorLerp colorLerp;
+
+	void Start () {
+        colorLerp = transform.parent.gameObject.GetComponent<TextMeshColorLerp>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            colorLerp.startColorChange(1);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            colorLerp.startColorChange(-1);
+        }
+    }
+}
---
+++ LostInTransmission/Assets/SetText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetText : MonoBehaviour {
+
+    // Use this for initialization
+    public string[] lines;
+    public int poemNumber;
+    public int lineNumber;
+    private TextMesh text;
+	void Start () {
+        text = GetComponent<TextMesh>();
+        text.text = "";
+        text.text += lines[poemNumber * 3 + lineNumber];
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
