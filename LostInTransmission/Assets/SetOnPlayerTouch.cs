4b56652dd88c83ba5ebd387584e6c416054ae2f0
PGrad <paulgrad3rd@gmail.com>
2018-01-27 22:29:22 -0800

    Re-enable GUI in level2

---
+++ LostInTransmission/Assets/SetOnPlayerTouch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetOnPlayerTouch : MonoBehaviour {
+	private State state;
+	void Awake() {
+		state = GetComponent<State>();
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.tag == "Player")
+			state.setState(state.getState() ^ 1);
+	}
+}
