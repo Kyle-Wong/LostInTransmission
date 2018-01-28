cc238951a3b6f16f55a0162a798200d6bf6f578a
Kyle Wong <kylewonga@gmail.com>
2018-01-27 16:52:31 -0800

    Added end of level goal

---
+++ LostInTransmission/Assets/Scripts/SceneController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour {
+
+    // Use this for initialization
+    public GraphicColorLerp colorLerp;
+
+	void Awake () {
+        if (true || GameData.playSceneIntro)
+        {
+            colorLerp.GetComponent<Image>().enabled = true;
+            colorLerp.setColors(Color.black, new Color(0, 0, 0, 0));
+            colorLerp.startColorChange();
+        } else
+        {
+            colorLerp.GetComponent<Image>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public static IEnumerator transitionThenLoad(string sceneName, float delay, bool introTransition)
+    {
+        GraphicColorLerp colorLerp = GameObject.Find("Overlay").GetComponent<GraphicColorLerp>();
+        colorLerp.GetComponent<Image>().enabled = true;
+        colorLerp.duration = delay;
+        colorLerp.setColors(new Color(0, 0, 0, 0), Color.black);
+        colorLerp.startColorChange();
+        
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
